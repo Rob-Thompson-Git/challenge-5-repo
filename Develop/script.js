8e8b04bbacc57534f2587ef913353baab9361ae0
RobThompson <rob.thomp15@gmail.com>
2022-09-10 18:00:47 -0500

    successfully linked time blocks with current time to set textarea color

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,30 @@
+//Gets current date at top of page
 $(document).ready(function() {
     $('#top-date').text(moment().format("dddd, MMMM Do, YYYY"));
 }); 
 
+
 // Trying to create currentTime variable to compare to past, present, future
-let timeBlockHour = parseInt($(this).prop("id"));
-console.log(timeBlockHour);
-console.log($(this));
+let currentHour = moment().format("HH");
+$(".hour").each(function () {
+    let blockHour = parseInt($(this).attr("data-time"));
+    
+    // console.log(timeBlockHour);
+    if (blockHour < currentHour) {
+        $(this).siblings("textarea").addClass("past");
+        $(this).siblings("textarea").removeClass("present");
+    } else if (blockHour > currentHour) {
+        $(this).siblings("textarea").addClass("future");
+        $(this).siblings("textarea").removeClass("past");
+    } else {
+        $(this).siblings("textarea").addClass("present");
+        $(this).siblings("textarea").removeClass("future");
+    }
+});
 
 
-// Trying to save userInput into console log when saveBtn is clicked
+
+// Trying to save userInput into local storage log when saveBtn is clicked
 $('.saveBtn').on('click', function(event) {
     event.preventDefault();
     console.log(event.target);
@@ -17,6 +33,7 @@ $('.saveBtn').on('click', function(event) {
     var formattedIdFinder = "#text-area-hour-" + hourClickedToSave; // =>"#text-area-hour-9"
     var textAreaInputValue = $(formattedIdFinder).val();
     console.log(textAreaInputValue);
+    
 }); 
 
 
